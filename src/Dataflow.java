817b3341580eda9b6ef4fa68aa20ded13ca7653d
Jiaqi-Desktop201 <haojiaqi666@gmail.com>
2019-03-03 09:04:23 -0800

    start

---
+++ src/Dataflow.java
@@ -0,0 +1,13 @@
+import soot.*;
+import soot.jimple.*;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+
+import java.util.*;
+
+public class Dataflow {
+
+    protected void internalTransform(String arg0, Map arg1) {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import soot.*;
+import soot.options.Options;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String classPath = "inputs";
+        configureSoot(classPath);// configure soot
+        Scene.v().loadNecessaryClasses(); // load all the library and dependencies for given program
+        Dataflow mDataflow = new Dataflow();
+
+    }
+
+    public static void configureSoot(String classpath) {
+        Options.v().set_whole_program(true);  // process whole program
+        Options.v().set_allow_phantom_refs(true); // load phantom references
+        Options.v().set_prepend_classpath(true); // prepend class path
+        Options.v().set_src_prec(Options.src_prec_class); // process only .class files, change here to process other IR or class
+        Options.v().set_output_format(Options.output_format_jimple); // output jimple format, change here to output other IR
+        ArrayList<String> list = new ArrayList<>();
+        list.add(classpath);
+        Options.v().set_process_dir(list); // process all .class files in directory
+        Options.v().setPhaseOption("cg.spark", "on"); // use spark for call graph
+    }
+
+}
